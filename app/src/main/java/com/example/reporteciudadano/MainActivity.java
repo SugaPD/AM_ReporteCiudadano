e9b1fa481f16d78d32b1d5f24c91dc90f4ac010c
Suga <ellelol@outlook.com>
2026-05-10 21:52:45 -0700

    Menu principal

--- app/src/main/java/com/example/reporteciudadano/MainActivity.java
+++ app/src/main/java/com/example/reporteciudadano/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.reporteciudadano;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnReporte, btnContacto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReporte = findViewById(R.id.btnReporte);
+        btnContacto = findViewById(R.id.btnContacto);
+
+        btnReporte.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, ReporteActivity.class);
+            startActivity(intent);
+
+        });
+
+        btnContacto.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, ContactoActivity.class);
+            startActivity(intent);
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/reporteciudadano/ReporteActivity.java
@@ -0,0 +1,13 @@
+package com.example.reporteciudadano;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReporteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
